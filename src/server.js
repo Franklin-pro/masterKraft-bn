ab6fc9ffd84bc27a23715c8705c8a5eec55d8434
ATLP <atlp@ATLPs-MacBook-Pro.local>
2024-03-14 21:50:25 +0200

    sevr

---
+++ src/server.js
@@ -0,0 +1,26 @@
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+import  express  from "express"
+import bodyParser from "body-parser"
+import cors from "cors"
+
+
+dotenv.config()
+
+const Master=express()
+Master.use(bodyParser.json())
+Master.use(cors())
+
+const port=process.env.PORT
+const db=process.env.DATABASE
+
+Master.listen(port,()=>{
+    console.log(`port running on ${port}`)
+})
+
+mongoose.connect(db).then(()=>{
+    console.log("successfuly database connected...")
+})
+.catch((error)=>{
+    console.log(error)
+})
