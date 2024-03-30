da1b59a61801705b6d9c7ac03b2666c8424e7a13
Franklin-Pro <franklinprogrammer@gmail.com>
2024-03-30 16:08:03 +0200

    good jobs

--- src/routers/index.js
+++ src/routers/index.js
@@ -4,6 +4,7 @@ import contactRouter from "./contactRouter.js"
 import videoRouter from "./videoRouter.js"
 import didyouRouter from "./didyouRouter.js"
 import productRouter from "./productRouter.js"
+import oder from './oderRouter.js'
 
 
 const router=express.Router()
@@ -12,5 +13,6 @@ router.use("/contact",contactRouter)
 router.use("/video",videoRouter)
 router.use("/didyou",didyouRouter)
 router.use("/product",productRouter)
+router.use("/order",oder)
 
 export default router
