7e56db081180083e2200473bf57b2ceda4329763
Franklin-Pro <franklinprogrammer@gmail.com>
2024-04-03 11:22:08 +0200

    good job on team

--- src/controller/tearmController.js
+++ src/controller/tearmController.js
@@ -1,36 +1,35 @@
-import Team from "../model/tearm.js";
-import cloudinary from '../utiles/cloud.js';
-import errormessage from '../utiles/errormessage.js';
-import successmessage from '../utiles/successmessage.js'
+import cloudinary from 'cloudinary';
+import Team from '../model/tearm.js';
+import errormessage from '../utiles/errormessage.js'; 
+import sucessmessage from '../utiles/successmessage.js';
 
 class OurTearm {
-    static async tearms (req,res){
-try {
-    if(!req.file){
-        return errormessage(res,401,`please choose image`)
+    static async tearms(req, res) {
+        try {
+            if (!req.file) {
+                return errormessage(res, 401, `Please choose an image`);
+            }
+            const result = await cloudinary.uploader.upload(req.file.path, {
+                folder: 'tearm'
+            });
+            const tearm = await Team.create({
+                teamImage: {
+                    public_id: result.public_id,
+                    url: result.secure_url,
+                },
+                personName: req.body.personName, // Access personName from req.body
+                personWork: req.body.personWork, // Access personWork from req.body
+            });
+            if (tearm) {
+                return sucessmessage(res, 201, `${req.body.personName} posted successfully`, tearm);
+            } else {
+                return errormessage(res, 401, `Posted failed`);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            return errormessage(res, 500, `Error: ${error.message}`);
+        }
     }
-    const result = await cloudinary.uploader.upload(req.file.path, {
-        folder: 'tearm'
-    });
-    const tearm = await Team.create({
-        teamImage: {
-            public_id: result.public_id,
-            url: result.secure_url,
-          },
-          personName:req.body.personName,
-          personWork:req.body.personWork,
-    })
-    if(tearm){
-        return successmessage(res,201,`${personName} posted successfully`,tearm)
-    }
-    else{
-        return errormessage(res,401,`posted failed`)
-    }
-} catch (error) {
-    console.error('Error:', error);
-    return errormessage(res, 500, `Error: ${error.message}`);
 }
-    }
-}
-export default OurTearm
 
+export default OurTearm;
