779c71efc913e8d92b3b2c300b58bf64b86711bc
kagabojaphet <iradukundajaphet59@gmail.com>
2024-04-05 09:36:59 +0200

    add

--- src/routers/oderRouter.js
+++ src/routers/oderRouter.js
@@ -2,7 +2,7 @@ import express from "express";
 import oderController from "../controller/orderController.js";
 import VerifyAccess from "../middlewares/velifyaccess.js";
 const router=express.Router()
-router.post("/:id",VerifyAccess("user"),oderController.orderingProduct)
+router.post("/:id",oderController.orderingProduct)
 router.delete("/",VerifyAccess("admin"),oderController.deleteOder)
 router.get("/",VerifyAccess("admin"),oderController.getoder)
 router.get("/:id",oderController.getOneOder)
