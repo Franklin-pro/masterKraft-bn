3c2e9c200358af031f0534a9ff5fdcdda0b3eaa8
ATLP <atlp@ATLPs-MacBook-Pro.local>
2024-04-12 09:45:27 +0200

    change database

--- src/server.js
+++ src/server.js
@@ -10,16 +10,21 @@ dotenv.config();
 
 const app = express();
 
-
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/videos', express.static(path.join(new URL('../uploads', import.meta.url).pathname)));
-app.use("/API", router);
 
+
+const currentModuleDirectory = path.dirname(new URL(import.meta.url).pathname);
+const uploadsDirectory = path.join(currentModuleDirectory, '../uploads');
+app.use('/videos', express.static(uploadsDirectory));
+
+
+app.use("/API", router);
 
 const port = process.env.PORT;
 const dbUri = process.env.DATABASE;
+
 mongoose.connect(dbUri)
     .then(() => {
         console.log("Successfully connected to the database.");
